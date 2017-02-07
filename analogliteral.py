259bee77a024012481ee1ced1ee778eb98422b23
dzdaniel84 <dzdaniel84@gmail.com>
2017-02-06 21:47:41 -0800

    Added doctests

--- analogliteral.py
+++ analogliteral.py
@@ -7,17 +7,38 @@ Since idea is not mine, feel free to do whatever with this code if you want.
 Only use {'O', 'L', '-', '|'} in your analog literals, please.
 """
 
+"""
+Returns the length of a line written in ASCII.
+
+Must include 'O', '-'.
+>>> area("O-----------O")
+11.0
+"""
 def length(str):
 	return str.count('-')
 
+"""
+Returns the area of a rectangle written in ASCII.
+
+Must include 'O', '-', '|'.
+>>> area("""
+O----O
+|    |
+|    |
+|    |
+|    |
+O----O
+""")
+16.0
+"""
 def area(str):
 	return (str.count('-') * str.count('|')) / 4
 
-def volume(str):
-	return (str.count('|') * str.count('-') * str.count('L'))/27
-
 """
-assert(volume("""
+Returns the volume of a rectangular prism written in ASCII.
+
+Must include 'O', 'L', '-', '|'.
+>>> volume("""
 O-----------O
 |L           L
 | L           L
@@ -29,16 +50,9 @@ O    O-----------O
    L |           |
     L|           |
      O-----------O
-""") == 176)
+""")
+176.0
+"""
+def volume(str):
+	return (str.count('|') * str.count('-') * str.count('L'))/27
 
-assert(area("""
-			 O----O
-			 |    |
-			 |    |
-			 |    |
-			 |    |
-			 O----O
-			 """) == 16)
-
-assert(length("O-----------O" == 11)
-""""""
